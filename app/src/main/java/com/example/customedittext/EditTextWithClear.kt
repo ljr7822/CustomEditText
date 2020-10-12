511d90c2dd44d65cbb1d83a9fbe66190a55e08e9
iwenLjr <1816807688@qq.com>
2020-10-12 21:45:55 +0800

    自定义文本编辑框，添加了一键删除、失去焦点删除图标隐藏

---
+++ app/src/main/java/com/example/customedittext/EditTextWithClear.kt
@@ -0,0 +1,68 @@
+package com.example.customedittext
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+
+/**
+ * 自定义的EditText
+ * @author iwen大大怪
+ * Create to 2020/10/12 21:17
+ * @JvmOverloads：函数重载
+ */
+class EditTextWithClear @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : androidx.appcompat.widget.AppCompatEditText(context, attrs, defStyleAttr) {
+
+    // 获得清除图标
+    private val iconDrawable = ContextCompat.getDrawable(context, R.drawable.ic_baseline_clear_24)
+
+    // 当文字改变时出现图标
+    override fun onTextChanged(
+        text: CharSequence?,
+        start: Int,
+        lengthBefore: Int,
+        lengthAfter: Int
+    ) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter)
+        toggleClearIcon()
+
+    }
+
+    // 重写触摸监听
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let { e ->
+            iconDrawable?.let {
+                if (e.action == MotionEvent.ACTION_UP
+                    && e.x > width - it.intrinsicWidth - 20
+                    && e.x < width + 20
+                    && e.y > height / 2 - it.intrinsicHeight / 2 - 20
+                    && e.y < height / 2 + it.intrinsicHeight / 2 + 20
+                ) {
+                    text?.clear()
+                }
+            }
+        }
+        performClick()
+        return super.onTouchEvent(event)
+    }
+
+    override fun performClick(): Boolean {
+        return super.performClick()
+    }
+
+    override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect)
+        toggleClearIcon()
+    }
+
+    // 显示图标函数
+    private fun toggleClearIcon() {
+        val icon = if (isFocused && text?.isNotEmpty() == true) iconDrawable else null
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, icon, null)
+    }
+}
